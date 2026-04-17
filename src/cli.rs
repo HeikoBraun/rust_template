f4f9e290bed3e09b9306308e3d7e8806a25daaa1
HeikoBraun <braun.heiko+github@proton.me>
2026-04-17 22:21:39 +0200

    --about now also works with mandatory argument filename

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about="<ToDo: description of program>", long_about = None)]
 pub struct Cli {
+    /// filename
+    #[arg(help = "filename to be processed", required_unless_present = "about")]
+    pub filename: Option<String>,
+
     /// toml
     #[arg(short = 't', long = "toml", help = "Toml file for config.")]
     pub toml_file: Option<String>,
@@ -20,7 +24,7 @@ pub struct Cli {
     #[arg(
         long = "about",
         default_value_t = false,
-        help = "More details about this program"
+        help = "If set, show details of this programs build and exit."
     )]
     pub about: bool,
 }
