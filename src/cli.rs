26b0e88fbc74e8cdb6d2890c118717be1ae22c22
HeikoBraun <braun.heiko+github@proton.me>
2026-07-05 22:42:54 +0200

    added hidden option and help-all option to show also hidden options

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[command(author, version, about="<ToDo: description of program>", long_about = None)]
 pub struct Cli {
     /// filename
-    #[arg(help = "filename to be processed", required_unless_present = "about")]
+    #[arg(help = "filename to be processed", required_unless_present_any = ["about","help_all"])]
     pub filename: Option<String>,
 
     /// toml
@@ -27,4 +27,18 @@ pub struct Cli {
         help = "If set, show details of this programs build and exit."
     )]
     pub about: bool,
+
+    /// hidden option
+    #[arg(
+        short,
+        long,
+        hide = true,
+        default_value_t = 1,
+        help = "Runs in parallel, 0 = num of cores, 1 = no parallelism"
+    )]
+    pub parallel: u8,
+
+    /// Show full help including hidden options
+    #[arg(long = "help-all", default_value_t = false, help = "Show all options")]
+    pub help_all: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use crate::main_functions::print_about;
-use clap::Parser;
+use crate::about_functions::print_about;
+use clap::{CommandFactory, Parser};
 use env_logger::Env;
 use log::debug;
-use std::process::exit;
 
+mod about_functions;
 mod cli;
-mod main_functions;
 
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let cli_args = cli::Cli::parse();
 
+    if cli_args.help_all {
+        // Build a fresh command and un-hide all args
+        let mut cmd = cli::Cli::command();
+
+        // Unhide everything
+        for id in cmd
+            .get_arguments()
+            .map(|a| a.get_id().clone())
+            .collect::<Vec<_>>()
+        {
+            cmd = cmd.mut_arg(id, |a| a.hide(false));
+        }
+        cmd.print_help().unwrap();
+        return;
+    }
+
     if cli_args.about {
         print_about();
-        exit(0);
+        return;
     }
     debug!("cli_args: {:?}", cli_args);
 
